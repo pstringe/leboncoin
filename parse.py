9eb079150428e54f0c47d8812206338230929881
Poitier Stringer <pstringe@e1z2r9p14.42.us.org>
2018-12-02 21:39:05 -0800

    retrieving rooms and surface area

--- parse.py
+++ parse.py
@@ -10,10 +10,11 @@ class Listing(object):
     location = {}
     zipcode = ""
     agency_or_individual = False
-    no_of_rooms = 0
+    rooms = 0
     superficy = 0
     ges = ""
     energy = ""
+    surface_area = 0
     pics = []
 
     def __init__(self, file):
@@ -23,11 +24,13 @@ class Listing(object):
         self.data = BeautifulSoup(html, 'html.parser')
 
     def print(self):
-        print('title:\t{}'.format(self.raw["name"]))
-        print('price:\t{}'.format(self.price))
-        print('location:\t{}'.format(self.location))
-        print('ges:\t{}'.format(self.ges))
-        print('energy rating:\t{}'.format(self.energy))
+        print('title: {}'.format(self.raw["name"]))
+        print('price: {}'.format(self.price))
+        print('location: {}'.format(self.location))
+        print('ges: {}'.format(self.ges))
+        print('energy class: {}'.format(self.energy))
+        print('rooms: {}'.format(self.rooms))
+        print('surface area: {}'.format(self.surface_area))
     
     def getPrice(self):
         self.price = self.data.body.find("span", {"class": "_1F5u3"}).contents[1]
@@ -37,6 +40,14 @@ class Listing(object):
         self.location["city"] = location_data[1]
         self.location["zip"] = location_data[7]
 
+    def getRooms(self):
+        criterea = self.data.body.find_all("div", {"class" : "_3Jxf3"})
+        self.rooms = criterea[1].contents[0]
+
+    def getSurfaceArea(self):
+        criterea = self.data.body.find_all("div", {"class" : "_3Jxf3"})
+        self.surface_area = criterea[2].contents[0].split(" ", 1)[0]
+        
     #needs to be narrowed down
     def getGes(self):
         self.ges = self.data.body.find("div", {"class" : "_1sd0z"}).contents[0]
@@ -44,6 +55,7 @@ class Listing(object):
     #needs to be narrowed
     def getEnergy(self):
         self.energy = self.data.body.find("div", {"class" : "_2RkBA _1jK6m _1sd0z"})
+    
 
 
 #create new listing
@@ -51,18 +63,21 @@ listing = Listing("listing_sample.html")
 
 #retrieve price
 listing.getPrice()
-listing.print()
 
 #retrieve ges
 listing.getGes()
-listing.print()
 
 #retrive energy rating
 listing.getEnergy()
-listing.print()
 
 #retrieve location
 listing.getLocation()
-listing.print()
 
-#retrieve
+#retrieve number of rooms
+listing.getRooms()
+
+#retrieve Surface Area
+listing.getSurfaceArea()
+
+#print the listing data
+listing.print()

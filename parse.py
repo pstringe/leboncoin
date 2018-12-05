b2825fdeffa4f9ab4519653b465eb3f38939fccc
Poitier Stringer <pstringe@e1z3r1p12.42.us.org>
2018-12-04 18:04:20 -0800

    serialization works

--- parse.py
+++ parse.py
@@ -6,11 +6,12 @@ Poitier Stringer
 
 import  sys
 from geopy.geocoders import Nominatim
-#import  geocoder
 import  time
 from bs4 import BeautifulSoup
+from json import JSONEncoder
+import json
 
-#definition for listing class
+#Listing class
 class Listing(object):
     raw = {}
     data = {}
@@ -19,8 +20,6 @@ class Listing(object):
         self.raw["name"] = file
         self.raw["file"] =  open(self.raw["name"], 'r')
         self.name = self.raw["name"]
-        html = self.raw["file"].read()
-        self.data = BeautifulSoup(html, 'html.parser')
 
     def print(self):
         print('\n')
@@ -33,81 +32,106 @@ class Listing(object):
         print('surface area: {}'.format(self.surface_area))
         print('pictures: {}'.format(self.pics))
 
-    def parseInfo(self):        
-        self.setPrice()
-        self.setGes()
-        self.setEnergy()
-        self.setLocation()
-        self.setRooms()
-        self.setSurfaceArea()
-        self.setPics()
+    def parseInfo(self, data):        
+        self.setPrice(data)
+        self.setGes(data)
+        self.setEnergy(data)
+        self.setLocation(data)
+        self.setRooms(data)
+        self.setSurfaceArea(data)
+        self.setPics(data)
         
-    def setPrice(self):
-        self.price = self.data.body.find("span", {"class": "_1F5u3"}).contents[1]
+    def setPrice(self, data):
+        self.price = None
+        if (data.body):
+            self.price = data.body.find("span", {"class": "_1F5u3"}).contents[1]
     
-    def setLocation(self):
-        location_data = self.data.body.find("div", {"class" : "_1aCZv"}).span.contents
-        self.location = {}
-        self.location["city"] = location_data[1]
-        self.location["zip"] = location_data[7]
-        self.location["coords"] = geoCode(self.location)
+    def setLocation(self, data):
+        self.location = None
+        if (data.body):
+            location_data = data.body.find("div", {"class" : "_1aCZv"}).span.contents
+            self.location = {}
+            self.location["city"] = location_data[1]
+            self.location["zip"] = location_data[7]
+            self.location["coords"] = geoCode(self.location)
 
-    def setRooms(self):
+    def setRooms(self, data):
         self.rooms = None
-        criteria = self.data.body.find("div", {"data-qa-id" : "criteria_container"})
-        criteria = criteria.find("div", {"data-qa-id" : "criteria_item_rooms"})
-        if (criteria):
-            criteria = criteria.find("div", {"class" : "_3Jxf3"})
-            self.rooms = criteria.contents[0]
-
-    def setSurfaceArea(self):
-        self.surface_area = None
-        criteria = self.data.body.find("div", {"data-qa-id" : "criteria_container"})
-        if (criteria):
-            criteria = criteria.find("div", {"data-qa-id" : "criteria_item_square"})
+        if (data.body):
+            criteria = data.body.find("div", {"data-qa-id" : "criteria_container"})
+            criteria = criteria.find("div", {"data-qa-id" : "criteria_item_rooms"})
             if (criteria):
                 criteria = criteria.find("div", {"class" : "_3Jxf3"})
-                self.surface_area = criteria.contents[0].split(" ", 1)[0]
+                self.rooms = criteria.contents[0]
+
+    def setSurfaceArea(self, data):
+        self.surface_area = None
+        if (data.body):
+            criteria = data.body.find("div", {"data-qa-id" : "criteria_container"})
+            if (criteria):
+                criteria = criteria.find("div", {"data-qa-id" : "criteria_item_square"})
+                if (criteria):
+                    criteria = criteria.find("div", {"class" : "_3Jxf3"})
+                    self.surface_area = criteria.contents[0].split(" ", 1)[0]
         
-    def setGes(self):
+    def setGes(self, data):
         self.ges = None
-        criteria = self.data.body.find("div", {"class" : "_277XW"})
-        criteria = criteria.find("div", {"data-qa-id" : "criteria_item_ges"})
-        if (criteria and criteria.find("div", {"class" : "_1sd0z"})):
-            self.ges = criteria.find("div", {"class" : "_1sd0z"}).contents[0]
+        if (data.body):
+            criteria = data.body.find("div", {"class" : "_277XW"})
+            criteria = criteria.find("div", {"data-qa-id" : "criteria_item_ges"})
+            if (criteria and criteria.find("div", {"class" : "_1sd0z"})):
+                self.ges = criteria.find("div", {"class" : "_1sd0z"}).contents[0]
     
-    def setEnergy(self):
+    def setEnergy(self, data):
         self.energy_class = None
-        criteria = self.data.body.find("div", {"class" : "_277XW"})
-        criteria = criteria.find("div", {"data-qa-id" : "criteria_item_energy_rate"})
-        if (criteria and  criteria.find("div", {"class" : "_1sd0z"})):
-            self.energy_class = criteria.find("div", {"class" : "_1sd0z"}).contents[0] 
+        if (data.body):
+            criteria = data.body.find("div", {"class" : "_277XW"})
+            criteria = criteria.find("div", {"data-qa-id" : "criteria_item_energy_rate"})
+            if (criteria and  criteria.find("div", {"class" : "_1sd0z"})):
+                self.energy_class = criteria.find("div", {"class" : "_1sd0z"}).contents[0] 
    
     #need to correct this
-    def setPics(self):
+    def setPics(self, data):
         self.pics = None
-        if (self.data.find("div", {"class": "_2x8BQ"})):
-            self.pics = self.data.find("div", {"class": "_2x8BQ"}).find("img")['src']
+        if (data.find("div", {"class": "_2x8BQ"})):
+            self.pics = data.find("div", {"class": "_2x8BQ"}).find("img")['src']
+
+#custom json encoder
+class ListingEncoder(JSONEncoder):
+    def default(self, o):
+        return o.__dict__
 
 #retrieve longitude/lattitudes
 def geoCode(loc):
     time.sleep(1)
     g = Nominatim(user_agent="leboncoin scraper")
     coords = g.geocode(loc["zip"])
-    return({coords.longitude, coords.latitude})
+    return([coords.longitude, coords.latitude])
+
+    
 
 #loop through input files, store listing objects in an array, listings
 listings = []
+outputFile = open("out.json", "w")
+
 for arg in sys.argv[1:]:
     listing = Listing(arg)
+    #init data
+    html = listing.raw["file"].read()
+    data = BeautifulSoup(html, 'html.parser')
     
     #initialize listing object
     listings.append(listing)
     
     #retrieve document info
-    listings[-1].parseInfo()
-    listings[-1].print()
+    listings[-1].parseInfo(data)
+    
+    #save data
+    ListingEncoder().encode(listings[-1])
+    s = json.dumps(listings[-1], cls=ListingEncoder)
+    print(s)
+    outputFile.write(s)
     
 #make sure listings are being stored correctly
 for listing in listings:
-    listing.print()
+    print(json.dumps(listing))

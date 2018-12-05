d2f85af288b707484b9f25ff35b61c5851f05cc9
Poitier Stringer <pstringe@e1z3r1p12.42.us.org>
2018-12-05 08:30:53 -0800

    added record info

--- load.py
+++ load.py
@@ -1,8 +1,9 @@
+import sys
 import json
 from pprint import pprint
 
 #load file
-with open('sample.json') as f:
+with open(sys.argv[1]) as f:
     data = f.readlines()
 
 #load json
@@ -11,6 +12,10 @@ for datum in data:
     print(datum)
     records.append(json.loads(datum))
 
+complete = []
 #Here are your objects
 for r in records:
-    pprint(r)
+    if (r["location"] and r["location"]["coords"]):
+        complete.append(r)
+pprint("complete records:{}".format(len(complete)))
+pprint("total records:{}".format(len(data)))

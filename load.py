9496f1a838de07a54a3765ab87fe04909a4b0091
Poitier Stringer <pstringe@e1z3r1p12.42.us.org>
2018-12-05 09:02:55 -0800

    encapsulated load functions

--- load.py
+++ load.py
@@ -2,20 +2,32 @@ import sys
 import json
 from pprint import pprint
 
-#load file
-with open(sys.argv[1]) as f:
-    data = f.readlines()
+class   JsonData():
+    def __init__(self, jsonFile):
+        self.loadFile(jsonFile);    
+        self.loadRecords();
+        self.setComplete();
 
-#load json
-records = []
-for datum in data:
-    print(datum)
-    records.append(json.loads(datum))
+    def loadFile(self, jsonFile):
+        self.file = jsonFile
+        with open(self.file) as f:
+            self.data = f.readlines()
 
-complete = []
-#Here are your objects
-for r in records:
-    if (r["location"] and r["location"]["coords"]):
-        complete.append(r)
-pprint("complete records:{}".format(len(complete)))
-pprint("total records:{}".format(len(data)))
+    def loadRecords(self):
+        self.records = []
+        for datum in self.data:
+            print(datum)
+            self.records.append(json.loads(datum))
+
+    def setComplete(self):
+        self.complete = [] 
+        for r in self.records:
+            if (r["location"] and r["location"]["coords"]):
+                self.complete.append(r)
+
+if (sys.argv[1] == "load"):
+    if (sys.argv[2]):
+        data = JsonData(sys.argv[2])
+
+pprint("complete records:{}".format(len(data.complete)))
+pprint("total records:{}".format(len(data.records)))

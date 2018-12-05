56ac73e8461a7be6a7d0123b4b08e86c0834c631
Poitier Stringer <pstringe@e1z3r1p12.42.us.org>
2018-12-05 10:59:34 -0800

    load works

--- load.py
+++ load.py
@@ -8,6 +8,10 @@ class   JsonData():
         self.loadRecords();
         self.setComplete();
 
+    def show(self):
+        pprint("complete records:{}".format(len(self.complete)))
+        pprint("total records:{}".format(len(self.records)))
+    
     def loadFile(self, jsonFile):
         self.file = jsonFile
         with open(self.file) as f:
@@ -24,10 +28,38 @@ class   JsonData():
         for r in self.records:
             if (r["location"] and r["location"]["coords"]):
                 self.complete.append(r)
+    
+    #merge two JsonData objects into cureent object
+    def mergeComplete(self, d1, d2):
+        for d1 in d1.complete:
+            if (d1 not in d2.complete):
+                self.records.append(d1)
+        for d2 in d2.complete:
+            if (d2 not in d1.complete):
+                self.records.append(d2)
+        self.setComplete()
+    
+d1 = None
+d2 = None
+d3 = None
+data = None
 
 if (sys.argv[1] == "load"):
     if (sys.argv[2]):
         data = JsonData(sys.argv[2])
+elif (sys.argv[1] == "merge"):
+    if (sys.argv[2] and sys.argv[3]):
+        d1 = JsonData(sys.argv[1])
+        d2 = JsonData(sys.argv[2])
+        res = sys.argv[3] if JsonData(sys.argv[3]) else JsonData("out.json")
+else:
+    pass
 
-pprint("complete records:{}".format(len(data.complete)))
-pprint("total records:{}".format(len(data.records)))
+if (data):
+    data.show()
+if (d1):
+    d1.show()
+if (d2):
+    d2.show()
+if (d3):
+    d3.show()

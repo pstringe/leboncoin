cfb5d60e6ccddea33ec2bb0881086c919c889eaf
Poitier Stringer <pstringe@e1z3r1p12.42.us.org>
2018-12-04 19:34:27 -0800

    loading prog loads json

---
+++ load.py
@@ -0,0 +1,17 @@
+import json
+from pprint import pprint
+
+#load file
+with open('sample.json') as f:
+    data = f.readlines()
+
+#load json
+records = []
+for datum in data:
+    print(datum)
+    records.append(json.loads(datum))
+    pprint(datum)
+
+#Here are your objects
+for r in records:
+    pprint(r)
